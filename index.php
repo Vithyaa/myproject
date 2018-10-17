0188a8cbb8662990d20698dd6dbcd4308209f54f
Vithyaa <42999422+Vithyaa@users.noreply.github.com>
2018-10-17 10:06:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+date_default_timezone_set('UTC');  
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii-1.1.15/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+$debug=dirname(__FILE__).'/protected/config/debug.php';
+
+include ($debug);
+
+require_once($yii);
+$app = Yii::createWebApplication($config);
+
+// Yii::import("application.vendors.*"); 
+Yii::import("application.components.XZendAutoLoader", true); 
+Yii::registerAutoloader(array('XZendAutoLoader', 'loadClass') , true);
+
+$app->run(); 
