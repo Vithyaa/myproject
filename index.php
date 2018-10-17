cbda32e08604f8b99eed17db8ccce4b59e8cb00b
Vithyaa <vithyashanthi@agilecyber.solutions>
2018-10-17 10:16:30 +0530

    Changes made in index file of the project

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 date_default_timezone_set('UTC');  
 // change the following paths if necessary
+
+
 $yii=dirname(__FILE__).'/../yii-1.1.15/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 $debug=dirname(__FILE__).'/protected/config/debug.php';
